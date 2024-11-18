cc11d5efb43f4698982374a7ee8023f53f0688a6
gunaseelan <gunaon2509@gmail.com>
2024-11-18 18:16:28 +0530

    feat: @kanalabs/kana-widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Widget from "./components/Widget";
+import {WidgetPage} from "./components/Widget";
 
 export default function Home() {
   return (
     <div className="absolute inset-0 flex flex-col place-items-center">
-          <Widget />
+          <WidgetPage />
     </div>
   );
 }
