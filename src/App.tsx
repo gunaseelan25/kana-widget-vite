76e2d6a03671ea2d7b5d4f1c71eafc121b972f08
gunaseelan <gunaon2509@gmail.com>
2024-11-18 17:44:18 +0530

    feat: updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import Widget from "./components/Widget";
+import { lazy, Suspense } from 'react';
+
+const Widget = lazy(() => import('./components/Widget'));
 
 export default function Home() {
   return (
     <div className="absolute inset-0 flex flex-col place-items-center">
-          <Widget />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Widget />
+      </Suspense>
     </div>
   );
 }
